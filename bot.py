7b93f5f0d0f048598219879f9ff067d881cafd79
FishNatural <pilotmisha@gmail.com>
2020-08-31 19:15:12 +0300

    '9'

--- bot.py
+++ bot.py
@@ -14,6 +14,15 @@ bot.
 """
 
 import logging
+import os
+
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
